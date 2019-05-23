d48ecc64cdd34c9c951a650a55e0fcc9cb09cdde
[Dorijan] <[dorijan11@gmail.com]>
2019-05-23 21:18:14 +0200

    Updates

---
+++ precache-manifest.971c7d39e1d8afd78e95701142f31f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd14fe76a0126048d99fb63b54295deb",
+    "url": "/Meteorite-Landings/index.html"
+  },
+  {
+    "revision": "7c4e172ade45e257026a",
+    "url": "/Meteorite-Landings/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d35366af77e9c5663669",
+    "url": "/Meteorite-Landings/static/js/2.7a7262d8.chunk.js"
+  },
+  {
+    "revision": "7c4e172ade45e257026a",
+    "url": "/Meteorite-Landings/static/js/main.22527bc7.chunk.js"
+  },
+  {
+    "revision": "cc14d24b250810f5e323",
+    "url": "/Meteorite-Landings/static/js/runtime~main.64fba6ad.js"
+  }
+]);
