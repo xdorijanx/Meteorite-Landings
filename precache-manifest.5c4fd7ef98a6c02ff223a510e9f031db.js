3e8cc0b594b96d14edae1f3661cc5b02d82050a9
[Dorijan] <[dorijan11@gmail.com]>
2019-05-26 21:24:05 +0200

    Updates

---
+++ precache-manifest.5c4fd7ef98a6c02ff223a510e9f031db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa37bb3c8067c652c1d62c0d5a9c125",
+    "url": "/Meteorite-Landings/index.html"
+  },
+  {
+    "revision": "b7ed4dd158108f738497",
+    "url": "/Meteorite-Landings/static/css/main.fde74cc7.chunk.css"
+  },
+  {
+    "revision": "38f53b0fd2febe00b110",
+    "url": "/Meteorite-Landings/static/js/2.27f7e52a.chunk.js"
+  },
+  {
+    "revision": "b7ed4dd158108f738497",
+    "url": "/Meteorite-Landings/static/js/main.d0ebfd44.chunk.js"
+  },
+  {
+    "revision": "cc14d24b250810f5e323",
+    "url": "/Meteorite-Landings/static/js/runtime~main.64fba6ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Meteorite-Landings/precache-manifest.4627fcb980ffe59cf5b470b9603806a3.js"
+  "/Meteorite-Landings/precache-manifest.5c4fd7ef98a6c02ff223a510e9f031db.js"
 );
 
 self.addEventListener('message', (event) => {
